8d04e8abadd2e3707412370be0b8a745df9a39af
Tom Dwyer <tom.uwe@gmail.com>
2026-04-02 00:01:57 +0000

    simple.html

--- web/src/simpleCoordinator.tsx
+++ web/src/simpleCoordinator.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SimpleCoordinatorApp from "./SimpleCoordinatorApp";
+import SimpleAppShell from "./SimpleAppShell";
 import "./styles.css";
 
 document.body.classList.add("simple-page");
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SimpleCoordinatorApp />
+    <SimpleAppShell initialRole="coordinator" />
   </React.StrictMode>,
 );
